8e868f7733e9dcb0c8d978e8122b3045bb35bb58
noorulshaj1999 <43335721+noorulshaj1999@users.noreply.github.com>
2018-09-18 15:59:51 +0530

    Update check whether a number is odd or even.py

--- check whether a number is odd or even.py
+++ check whether a number is odd or even.py
@@ -2,6 +2,6 @@ num=float(input("enter a number:")
 if (num%2==0):
     print("number is even")
 elif (num<0):
-    print(number is invalid")
+    print("number is invalid")
 else:
     printf("number is odd")
