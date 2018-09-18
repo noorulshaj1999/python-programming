08063d5254d64a95673943fc7906d3ce73038a28
noorulshaj1999 <43335721+noorulshaj1999@users.noreply.github.com>
2018-09-18 15:42:31 +0530

    Update check whether a number is positive or negative or negative or zero.py

--- check whether a number is positive or negative or negative or zero.py
+++ check whether a number is positive or negative or negative or zero.py
@@ -1,7 +1,7 @@
-num = float(input("Enter a number: "))
-if num > 0:
-   print("Positive number")
-elif num == 0:
-   print("Zero")
+num=float(input("enter a number:))
+if num>0:
+    print("positive number")
+elif num=0:
+     print("zero")
 else:
-   print("Negative number")
+   print("negative number")             
