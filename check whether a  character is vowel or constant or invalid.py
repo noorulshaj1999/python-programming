d530cf670035705d8b08c83d84cefea534eacc5e
noorulshaj1999 <43335721+noorulshaj1999@users.noreply.github.com>
2018-09-18 16:10:43 +0530

    Create check whether a  character is vowel or constant or invalid.py

---
+++ check whether a  character is vowel or constant or invalid.py
@@ -0,0 +1,7 @@
+num=char(input("enter a number")
+if (num=a,e,i,o,u):
+    print("your input is vovel")
+elif (num=char):
+    print("your input is constant")
+else:
+    print('your input is invalid")
